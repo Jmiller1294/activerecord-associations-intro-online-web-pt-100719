ed9a5adc57d9491b41821f4a2c0d2c39cf77c459
Justin Miller <jayman1294@gmail.com>
2020-01-13 23:46:43 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
+ActiveRecord::Schema.define(version: 2020_01_13_234415) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 4) do
     t.string "name"
     t.integer "artist_id"
     t.integer "genre_id"
+  end
+
+  create_table "toys", force: :cascade do |t|
+    t.string "name"
+    t.string "size"
+    t.string "brand"
   end
 
 end
