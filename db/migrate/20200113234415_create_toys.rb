375bc3d76009b471e19ea3995ead0863b9fd43f3
Justin Miller <jayman1294@gmail.com>
2020-01-13 23:46:16 +0000

    Automatically backed up by Learn

--- db/migrate/20200113234415_create_toys.rb
+++ db/migrate/20200113234415_create_toys.rb
@@ -1,6 +1,6 @@
 class CreateToys < ActiveRecord::Migration[5.2]
   def change
-    create_table :toys |t|
+    create_table :toys do |t|
     t.string :name
     t.string :size 
     t.string :brand
